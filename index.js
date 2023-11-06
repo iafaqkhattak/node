db1914f7ebbafd709ca3326ac1b4b9166bde3d46
Muhammad Afaq <iafaqkhattak@gmail.com>
2023-11-06 23:23:33 +0500

    Express

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.get("/", (req, res, next) => {});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 app.post("/api/v1/register", (req, res) => {
   const userName = req.body.name;
   const userEmail = req.body.email;
-  const userPass = req.body.pass;
+  const userPass = req.body.password;
 
   res.json({
     success: true,
